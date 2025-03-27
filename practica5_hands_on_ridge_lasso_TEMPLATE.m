15f19a571d851a07f9804b449da1bf24a8753c42
CrashLogger <ealonso074@ikasle.ehu.eus>
2025-03-27 13:42:48 +0100

    2025-03-27 RIDGE reduction eta LASSO reduction
    Praktika honetan RIDGE eta LASSO reductionekin lan egin dugu, biak konparatuz eta erabilera kasuak aztertuz.

--- practica5_hands_on_ridge_lasso_TEMPLATE.m
+++ practica5_hands_on_ridge_lasso_TEMPLATE.m
@@ -2,7 +2,7 @@ function practica5_hands_on_ridge_lasso_TEMPLATE
 % Este script contiene la resolución del tutorial práctico del Tema 5
 % de la asignatura 'Técnicas de Inteligencia Artificial'
 
-load('..\..\0_MATERIAL\Bases_de_datos\data\MAT\Hitters');
+load('Hitters');
 % Nombre de las variables
 var_names = Hitters.Properties.VariableNames;
 
@@ -29,21 +29,27 @@ X = [Hitters{:,1:13} League_N Division_W Hitters{:,16:18} NewLeague_N];
 Y = Hitters{:,19};
 
 % Creamos matriz de valores posible para lambda RIDGE REGRESSION
-
+A = linspace(-2, 4, 100);
+lambda_grid = 10.^A;
 
 % RIDGE
+B = ridge(Y,X,lambda_grid, 0);
 
+subplot(2,1,1);
+plot(lambda_grid, B(2:end, :))
 
 grid on;
 xlabel('Lambda');
 ylabel('Coeficientes');
 title('RIDGE REGRESSION');
 
-% Creamos matriz de valores posible para lambda LASSO
-
+% Usamos la misma matriz de valores para LASSO
 
 % LASSO
+B = lasso(X,Y,"Lambda",lambda_grid);
 
+subplot(2,1,2);
+plot(lambda_grid, B(2:end, :))
 
 grid on;
 xlabel('Lambda');
@@ -55,83 +61,109 @@ pause;close;
 rng(1); % Fijamos semilla para la generación de números aleatorios
 
 % Partición no estratificada 50% train y 50% test
-
+hpartition = cvpartition(size(Hitters, 1), "holdOut", 0.5);
+pos_train = hpartition.training;
+pos_test = hpartition.test;
 
 % RIDGE REGRESSION
+% Lambda inventada
 lambda = 4;
+B = ridge(Y(pos_train),X(pos_train,:),4,0);
 
-
-
+ypred = B(1) + X(pos_test,:)*B(2:end);
+MSE_test = mean((Y(pos_test)-ypred).^2);
 
 fprintf('\nMSE test (RIDGE, lambda = %.1f) = %.0f \n',lambda,MSE_test);
 fprintf('MSE test del modelo nulo = %.0f \n',mean((mean(Y(pos_train))-Y(pos_test)).^2));
 
-
-
 lambda = 10^10;
+% Lambda grande -> Modelo nulo
 
-
+B = ridge(Y(pos_train),X(pos_train,:),lambda,0);
+ypred = B(1) + X(pos_test,:)*B(2:end);
+MSE_test = mean((Y(pos_test)-ypred).^2);
 
 
 fprintf('MSE test (RIDGE, lambda = %.0f) = %.0f \n',lambda,MSE_test);
 
-
-
-
 lambda = 0;
+% Lambda pequeña: Mínimos cuadráticos, igual a fitlm
 
+B = ridge(Y(pos_train),X(pos_train,:),lambda,0);
+ypred = B(1) + X(pos_test,:)*B(2:end);
+MSE_test = mean((Y(pos_test)-ypred).^2);
 
 
 fprintf('MSE test (RIDGE, lambda = %.0f) = %.0f \n',lambda,MSE_test);
 
 % least squares
 
-
+mdl = fitlm(X(pos_train,:), Y(pos_train));
+ypred = predict(mdl, X(pos_test,:));
+MSE_test = mean((Y(pos_test)-ypred).^2);
 
 fprintf('MSE test (least squares) = %.0f \n',MSE_test);
 
 % Con LASSO
 lambda = 4;
-
+[B, fitInfo] = lasso(X(pos_train, :), Y(pos_train), 'Lambda',lambda);
+% El campo "Intercept" de dentro de fitInfo es el Beta0
+ypred = fitInfo.Intercept + X(pos_test,:)*B;
+MSE_test_LASSO = mean((Y(pos_test)-ypred).^2);
 
 
 fprintf('\nMSE test (LASSO, lambda = %.0f) = %.0f \n',lambda,MSE_test_LASSO);
-
-
-
 
 % Usamos ahora 10 FOLD CV en entrenamiento para seleccionar lambda óptima
 % Creamos particiones
 rng(2);
 k = 10;
 
-
-
+c = cvpartition(length(Y(pos_train)), 'KFold', k);
+X1 = X(pos_train,:);
+Y1 = Y(pos_train);
 
 lambda_grid = linspace(0.01,100,100);
 lambda_grid_LASSO = linspace(0.01,100,100);
 CV_MSE=[];CV_MSE_LASSO=[];
+
 for aa = 1:k
+    pos_train_CV =  c.training(aa);
+    pos_test_CV  =  c.test(aa);
 
-
-
-
-
-
+    Xtrain = X1(pos_train_CV,:);
+    Ytrain = Y1(pos_train_CV);
+    Xtest = X1(pos_test_CV,:);
+    Ytest = Y1(pos_test_CV);
 
     % Para cada lambda, ajustamos y evaluamos los modelos
     for bb=1:length(lambda_grid) 
-        % RIDGE REGRESSION
-        
-        
+
+        B = ridge(Ytrain,Xtrain,lambda_grid(bb),0);
+        ypred = B(1) + Xtest*B(2:end);
+        MSE_test= mean((Ytest-ypred).^2);
+        CV_MSE(aa,bb) = MSE_test;
+
         % LASSO
+
+        [B, fitInfo] = lasso(Xtrain, Ytrain, 'Lambda',lambda_grid_LASSO(bb));
+        % El campo "Intercept" de dentro de fitInfo es el Beta0
+        ypred = fitInfo.Intercept + Xtest*B;
+        MSE_test_LASSO = mean((Ytest-ypred).^2);
+        CV_MSE_LASSO(aa, bb) = MSE_test_LASSO;
         
         
     end
     
 end
 
+CV_MSE
+CV_MSE_LASSO
 
+% Mediamos por columna -> Valor medio para cualquier lambda -> Buscamos el
+% mejor lambda
+[val, pos] = min(mean(CV_MSE));
+[val2, pos2] = min(mean(CV_MSE_LASSO));
 
 subplot(211);plot(lambda_grid,mean(CV_MSE,1));title('RIDGE');
 hold on;plot(lambda_grid(pos),val,'ro');hold off;
@@ -139,19 +171,19 @@ subplot(212);plot(lambda_grid_LASSO,mean(CV_MSE_LASSO,1));title('LASSO');
 hold on;plot(lambda_grid_LASSO(pos2),val2,'ro');hold off;
 pause;close;
 
-
-
 % Para RIDGE
-
-
-
-
-
+lambda_RIDGE = lambda_grid(pos);
+B = ridge(Y(pos_train),X(pos_train,:),lambda_RIDGE,0);
+ypred = B(1) + X(pos_test,:)*B(2:end);
+MSE_test_RIDGE= mean((Y(pos_test)-ypred).^2);
 
 % Para LASSSO
+lamda_LASSO = lambda_grid_LASSO(pos);
+[B, fitInfo] = lasso(X(pos_train,:), Y(pos_train), 'Lambda',lamda_LASSO);
+% El campo "Intercept" de dentro de fitInfo es el Beta0
+ypred = fitInfo.Intercept + X(pos_test,:)*B;
 
-
-
+MSE_test_LASSO = mean((Y(pos_test)-ypred).^2);
 
 
 fprintf('\n Ridge regression, CV lambda = %.2f, el error de test = %4.0f \n',lambda_RIDGE,MSE_test_RIDGE);
@@ -160,4 +192,5 @@ fprintf('\n LASSO, CV lambda = %.2f, el error de test = %4.0f \n',lambda_LASSO,M
 % Reajustamos el modelo usando todas las observaciones de las que disponemos 
 % y el valor de lambda seleccionado
 
-
+B_RIDGE = ridge(Y,X,lambda_RIDGE,0);
+[B_LASSO, fitInfo] = lasso(X, Y, 'Lambda',lamda_LASSO);
